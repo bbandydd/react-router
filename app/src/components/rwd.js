f0935a1a6d72c11b39b7618fc3c37eb1daaeae38
Andy Tsai <andy_tsai@wistron.com>
2016-04-15 17:04:45 +0800

    practice

--- app/src/components/rwd.js
+++ app/src/components/rwd.js
@@ -8,31 +8,8 @@ export default class Rwd extends React.Component{
 
     render(){
         return (
-            <div>
-                <div className="container">
-                    <div className="col-quarter box bg-gray"></div>
-                    <div className="col-three-quarter box bg-orange"></div>
-                </div>
-
-                <div className="container">
-                      <div className="row">
-                        <div className="col-onethird">
-                          <div className="box bg-gray">
-                            12312321
-                          </div>
-                        </div>
-                        <div className="col-onethird">
-                          <div className="box bg-gray">
-                            12312321
-                          </div>
-                        </div>
-                        <div className="col-onethird">
-                          <div className="box bg-gray">
-                            12312321
-                          </div>
-                        </div>
-                    </div>
-                </div>
+            <div className="box">
+                
             </div>
         )
     }
